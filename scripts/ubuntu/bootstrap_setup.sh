08c8452605fe3d839206cc889098d0420cecc9a5
ErikBongers <mail@erikbongers.net>
2026-08-05 18:41:20 +0200

    Update bootstrap to git clone as own user.

--- scripts/ubuntu/bootstrap_setup.sh
+++ scripts/ubuntu/bootstrap_setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # bootstrap me with:
-# curl -sSL https://raw.githubusercontent.com/ErikBongers/mopidy_eboback/refs/heads/master/scripts/ubuntu/bootstrap_setup.sh | sudo bash
+# curl -sSL https://raw.githubusercontent.com/ErikBongers/mopidy_eboback/refs/heads/master/scripts/ubuntu/bootstrap_setup.sh | bash
 
-mkdir -p /opt/mopidy-dev && cd /opt/mopidy-dev || exit
+sudo mkdir -p /opt/mopidy-dev && cd /opt/mopidy-dev || exit
 # Give your user ownership so you can clone repositories and write code safely
 REAL_USER="${SUDO_USER:-$(whoami)}"
-chown -R $REAL_USER:$REAL_USER /opt/mopidy-dev
+sudo chown -R $REAL_USER:$REAL_USER /opt/mopidy-dev
 
-apt install git
+sudo apt install git
 
 git clone https://github.com/ErikBongers/mopidy_eboback.git
 
 cd /opt/mopidy-dev/mopidy_eboback/ || exit
 
-find scripts -type f -exec chmod +x {} +
+sudo find scripts -type f -exec chmod +x {} +
