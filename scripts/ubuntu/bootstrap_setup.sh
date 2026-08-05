23f99998287bec40c3123d903452c9086ed3c512
ErikBongers <mail@erikbongers.net>
2026-08-05 18:10:33 +0200

    Create bootstrap_setup.sh - wip...

--- scripts/ubuntu/bootstrap_setup.sh
+++ scripts/ubuntu/bootstrap_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+# bootstrap me with:
+# curl -sSL https://raw.githubusercontent.com/ErikBongers/mopidy_eboback/refs/heads/master/scripts/ubuntu/bootstrap_setup.sh | sudo bash
 
-mkdir -p /opt/mopidy-dev && cd /opt/mopidy-server || exit
+mkdir -p /opt/mopidy-dev && cd /opt/mopidy-dev || exit
 # Give your user ownership so you can clone repositories and write code safely
 REAL_USER="${SUDO_USER:-$(whoami)}"
 chown -R $REAL_USER:$REAL_USER /opt/mopidy-dev
 
 apt install git
+
