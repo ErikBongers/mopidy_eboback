2d83df7cef283d9dc012b39b16c6539a0797361b
ErikBongers <mail@erikbongers.net>
2026-08-05 17:49:27 +0200

    Scripts...wip.

--- scripts/ubuntu/install_mopidy_dev.sh
+++ scripts/ubuntu/install_mopidy_dev.sh
@@ -12,10 +12,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 #mkdir -p /home/erik/mopidy-server && cd /home/erik/mopidy-server
 mkdir -p /opt/mopidy-dev && cd /opt/mopidy-server || exit
 # Give your user ownership so you can clone repositories and write code safely
-chown -R $ORIGINAL_USER:$ORIGINAL_USER /opt/mopidy-dev
-
-"$SCRIPT_DIR/create_mopidy_user.sh"
-"$SCRIPT_DIR/create_media_grp.sh"
+REAL_USER="${SUDO_USER:-$(whoami)}"
+chown -R $REAL_USER:$REAL_USER /opt/mopidy-dev
 
 python3 -m venv --system-site-packages venv
 
@@ -30,4 +28,7 @@ git clone
 cd eboback || exit
 source ~/mopidy-server/venv/bin/activate
 pip install .
+
+"$SCRIPT_DIR/create_mopidy_user.sh"
+"$SCRIPT_DIR/create_media_grp.sh"
 
